64b75008e7ca1c778fb316c6b1f1fc64531a904b
MatthewConklin <conklin4@illinois.edu>
2018-01-30 18:42:17 -0600

    We are done.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,12 +35,26 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                maze.turnRight();
+                if (maze.canMove() == false) {
+                    maze.turnLeft();
+                    if (maze.canMove() == false) {
+                        maze.turnLeft();
+                        if (maze.canMove() == false) {
+                            maze.turnLeft();
+                        }
+                    }
+                }
+                maze.move();
+            }
         }
 
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
-        }
-    }
+                    if (maze.isFinished()) {
+                        System.out.println("You solved the maze!");
+                    } else {
+                        System.out.println("Try again!");
+                    }
+                }
 }
+
